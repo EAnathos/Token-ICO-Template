1c5dcccd47b9c5039aa3ef7b767bb4ea2b103b0a
Anathos <83123402+EAnathos@users.noreply.github.com>
2023-04-12 13:16:51 +0200

    init files

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x78F5A9Ce7471F57F34cC56886aA67F3BF7f4eB33";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
